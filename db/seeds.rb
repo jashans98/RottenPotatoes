2040924bd0760d4766818b19485fb0a6909d661f
Jashan Shewakramani <jashan1998s@gmail.com>
2016-07-10 04:29:18 -0700

    Setup routes, model, and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed rotten potatoes with some initial movies
+
+seed_movies = [
+  {title: 'Aladdin', rating: 'G', release_date: '25-Nov-1992'},
+  {title: 'When Harry Met Sally', rating: 'R', release_date: '21-Jul-1989'},
+  {title: 'The Help', rating: 'PG-13', release_date: '10-Aug-2011'},
+  {title: 'Raiders of the Lost Ark', rating: 'PG', release_date: '12-Jun-1981'}
+]
+
+seed_movies.each do |movie|
+  Movie.create!(movie)
+end
